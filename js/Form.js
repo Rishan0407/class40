7509b7af939fe807df4d5ff53ccc7c2d0137ae93
Rishan0407 <bansi.gobse@gmail.com>
2020-04-27 19:56:57 +0530

    multicar

--- js/Form.js
+++ js/Form.js
@@ -1,6 +1,7 @@
 class Form {
 
   constructor() {
+    this.reset=createButton("Reset");
     this.input = createInput("Name");
     this.button = createButton('Play');
     this.greeting = createElement('h2');
@@ -19,6 +20,7 @@ class Form {
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
+    this.reset.position(displayWidth-100,50);
 2
     this.button.mousePressed(()=>{
       this.input.hide();
@@ -31,6 +33,10 @@ class Form {
       this.greeting.html("Hello " + player.name)
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
     });
+    this.reset.mousePressed(()=>{
+      game.update(0);
+      player.updateCount(0);
+    })
 
   }
 }
